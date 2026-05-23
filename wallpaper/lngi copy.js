f4b93700c2b4cc0e4104eac970ceec2a37a85875
MrRedSharkFan <haychiu2021@gmail.com>
2026-05-24 00:43:56 +0800

    adding another branch was a bad idea

---
+++ wallpaper/lngi copy.js
@@ -0,0 +1,151 @@
+var thing = 0
+
+
+function cmp_bms(x, y) {
+    //this shit about to work af
+    return x >= y
+}
+
+
+
+function gen_init_bms(x) {
+    var s = ""
+    var e = ""
+    for (var i = 0; i < x; i++) {
+        s = s + "0,"
+        e = e + "1,"
+    }
+    return "(" + s + "0)(" + e + "1)[60]"
+}
+
+
+
+function gen_init_bms(x) {
+    var s = ""
+    var e = ""
+    for (var i = 0; i < x; i++) {
+        s = s + "0,"
+        e = e + "1,"
+    }
+    return `(${s}0)(${e}1)[99]`
+}
+function lngi(prec = 80, x = get_lngi_from_time(Date.now())) {
+    var s = gen_init_bms(Math.floor(x))
+    var j = x % 1
+    var l = 0
+    var k = [1]
+    var m = 0
+    while (l < prec) {
+        if (j < 0.5) {
+            j = j * 2
+            k.push(0)
+            m++
+        } else {
+            j = (j - 0.5) * 2
+            k[k.length - 1]++
+            if (j == 0) {
+                l = 30
+            }
+        }
+        l++
+    }
+
+    var p = 0
+    for (var i = 0; i <= m; i++) {
+        var t = Bms.parse(s).s.length
+        var s = Bms.str2expand(s, k[i])
+        var u = Bms.parse(s).s.length
+        if (t - 1 >= u) {
+            break
+        }
+        p = p + k[i] + 1
+    }
+    if (i == m + 1) {
+        var v = Bms.parse(s)
+        var q = v.s
+        q.pop()
+        v.s = q
+        var s = v.toString()
+    }
+    return [s.slice(0, -4), Math.min(p, 53)]
+}
+
+dt = 1767900000000
+//dt = dt-8640000000
+dt = Date['UTC'](2026, 3, 2) - 86400 * 1000 * 100 - 8 * 60 * 1000 * 60
+//dt = dt -2225400000
+
+const I = 3 + 1 / 16 + 1 / 64 + 1 / 32768
+const S = 3.125
+
+function get_lngi_from_time(t) {
+    var t = t - dt
+    t = Math.log10(t / 86400000 + 1) / Math.log10(10) + 1
+    if (t > I) {
+        t = (t / I) ** 0.5 + I - 1
+        if (t > S) {
+            t = (t / S) ** 18 + S - 1
+        }
+    }
+    return t
+}
+
+function ch_inv(x, y, z) {
+    return (x + 1 - y) ** (1 / z) * y
+}
+
+function reverse_enginnering(x) {
+    if (x < I) { return ((10 ** (x - 1)) - 1) * 86400000 + dt }
+    if (x < S) { return ((10 ** (ch_inv(x, I, 0.5) - 1)) - 1) * 86400000 + dt }
+    else { return ((10 ** (ch_inv(ch_inv(x, S, 18), I, 0.5) - 1)) - 1) * 86400000 + dt }
+}
+
+function get_percent(x) { return Math.min(100, (((get_lngi_from_time(Date.now()) % (2 ** (-x + 1))) * 2 ** (x - 1)) * 100)) }
+
+function case_closed(x) {
+    if (get_percent() == 100) { return 0 }
+    var ss = Math.ceil(get_lngi_from_time(Date.now()) * (2 ** x / 2)) / (2 ** x / 2)
+    return ((reverse_enginnering(ss) - Date.now()) / 1000).toFixed(3)
+}
+
+
+function update() {
+    var w = lngi(55) //HOW TF DID A GLOBAL VARIABLE MAKE THIS 3-4X FASTER
+    document.getElementById("1").innerHTML = w[0]
+    document.getElementById("3").innerHTML = get_percent(w[1]).toFixed(3) + "% (" + case_closed(w[1]) + "s)"
+    document.getElementById("12").innerHTML = "Time elapsed: " + format_time((Date.now() - dt) / 1000)
+    document.getElementById("4").style.width = get_percent(w[1]) * 0.4 + "%"
+    document.getElementById("100").innerHTML = `${lngi(40, get_next_mile()[0])[0]}
+                                                <br><small>${get_next_mile()[2]}
+                                                <br>${(100 * ((get_lngi_from_time(Date.now()) - get_next_mile()[3]) / (get_next_mile()[0] - get_next_mile()[3]))).toFixed(5)}%</small>`
+    document.getElementById("time").innerHTML = `${new Date().toLocaleDateString()}<br><small>${new Date().toLocaleTimeString()}</small>`
+    fps = 1000 / (Date.now() - last_update)
+    last_update = Date.now()
+}
+
+function smallUpdate(ss) {
+    document.getElementById("11").innerHTML = thing == 0 ? cal(lngi(ss)[0]) : draw(lngi(ss)[0])
+}
+
+
+var fps = 0
+var last_update = Date.now()
+setInterval(update, 10, 1)
+
+function format_time(t) {
+    if (t < 0) { return format_time(-t) + " ago" }
+    if (t < 60) { return t.toFixed(2) + "s" }
+    if (t < 3600) { return Math.floor(t / 60) + "min " + format_time(t % 60) }
+    if (t < 86400) { return Math.floor(t / 3600) + "h " + format_time(t % 3600) }
+    if (t < 86400 * 365) { return Math.floor(t / 86400) + "d " + format_time(t % 86400) }
+    else { return Math.floor(t / 86400 / 365) + "y " + format_time(t % (86400 * 365)) }
+}
+
+function saveSettings() {
+    localStorage.setItem("bms-font", document.getElementById("font").value)
+}
+
+function getThen(t) {
+    var t = new Date(t + Date.now())
+    return t.toDateString() + " " + t.toLocaleTimeString()
+}
---
+++ wallpaper/misc_graphics copy.js
@@ -0,0 +1,10 @@
+function summon_droplets(t) {
+    for (var i = 0; i < t; i++){
+        var x = Math.random() * 100
+        var ti = Math.random() * 1 + 0.5
+        var td = Math.random()*4.5-9
+        document.getElementById("droplet").innerHTML = document.getElementById("droplet").innerHTML+`<div class="droplet" style="left: ${x}%; animation-duration: ${ti}s; animation-delay: ${td}s"></div>`
+    }
+}
+
+summon_droplets(25)
